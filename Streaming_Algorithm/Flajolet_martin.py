f3a046e419ba36a05e04e5221ef31c043cfd9bc2
Kaiyin Chan <87163239+carolyntw@users.noreply.github.com>
2022-11-16 15:52:33 -0600

    Update Flajolet_martin.py

--- Streaming_Algorithm/Flajolet_martin.py
+++ Streaming_Algorithm/Flajolet_martin.py
@@ -38,40 +38,37 @@ def myhashs(s):
     return result
 
 
-# file_name = 'users.txt'
-# stream_size = 300
-# num_to_ask = 30  
-# output_file = 'task2.csv'
+if __name__ == '__main__':
+    file_name = 'users.txt'
+    stream_size = 300
+    num_to_ask = 30  
+    output_file = 'task2.csv'
 
-file_name = sys.argv[1]
-stream_size = int(sys.argv[2])
-num_to_ask = int(sys.argv[3]) 
-output_file = sys.argv[4]
 
-bx = BlackBox()
-num_func = 50
-a_list, b_list = generateHashFunc(num_func)
-output = []
+    bx = BlackBox()
+    num_func = 50
+    a_list, b_list = generateHashFunc(num_func)
+    output = []
 
-for i in range(num_to_ask):
-    stream_users = bx.ask(file_name, stream_size)
-    ground_truth = len(set(stream_users))
-    longest_trailing_zeros = [0]*(len(a_list)+len(b_list))
-    for user in stream_users:
-        hash_v = myhashs(user)
-        len_zero = []
-        for v in hash_v:
-            bin_hashed = str(bin(v)[2:])
-            len_zero.append(len(bin_hashed)-len(bin_hashed.rstrip('0')))
-        longest_trailing_zeros = [max(longest_trailing_zeros[u], len_zero[u]) for u in range(len(len_zero))]
-    window_estimate = int(getMedian(2**k for k in longest_trailing_zeros))
-    output.extend((i,ground_truth,window_estimate))
+    for i in range(num_to_ask):
+        stream_users = bx.ask(file_name, stream_size)
+        ground_truth = len(set(stream_users))
+        longest_trailing_zeros = [0]*(len(a_list)+len(b_list))
+        for user in stream_users:
+            hash_v = myhashs(user)
+            len_zero = []
+            for v in hash_v:
+                bin_hashed = str(bin(v)[2:])
+                len_zero.append(len(bin_hashed)-len(bin_hashed.rstrip('0')))
+            longest_trailing_zeros = [max(longest_trailing_zeros[u], len_zero[u]) for u in range(len(len_zero))]
+        window_estimate = int(getMedian(2**k for k in longest_trailing_zeros))
+        output.extend((i,ground_truth,window_estimate))
 
-with open(output_file,'w+') as f:
-    f.write('Time,Ground Truth,Estimation\n')
-    for i in range(0,len(output),3):
-        f.write(str(output[i])+','+str(output[i+1])+','+str(output[i+2]))
-        f.write('\n')
+    with open(output_file,'w+') as f:
+        f.write('Time,Ground Truth,Estimation\n')
+        for i in range(0,len(output),3):
+            f.write(str(output[i])+','+str(output[i+1])+','+str(output[i+2]))
+            f.write('\n')
 
 
         
